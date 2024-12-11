81b4493660a164c76b360437f5ab78a757ca82f2
Dmitry3Lyalyuk <165943906+Dmitry3Lyalyuk@users.noreply.github.com>
2024-12-12 00:03:27 +0300

    RoleManege_1 appsettings

--- FamilyManager.Infrastructure/Migrations/20241211194524_InitialCreate.Designer.cs
+++ FamilyManager.Infrastructure/Migrations/20241211194524_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FamilyManager.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241209194655_AddIdentity")]
-    partial class AddIdentity
+    [Migration("20241211194524_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
